b2249ced54eea86e604132786c88b619bb7c69aa
caoxinyu <sxinyus@126.com>
2018-07-09 17:17:01 +0800

    make the form transparent

--- SyaringanWidget.cpp
+++ SyaringanWidget.cpp
@@ -33,18 +33,22 @@ SyaringanWidget::SyaringanWidget(QWidget *parent) :
     ui(new Ui::SyaringanWidget)
 {
     ui->setupUi(this);
+
+    connect(ui->lineEditInput, SIGNAL(textChanged(QString)), this, SLOT(textChangedSlot(QString)));
+    m_showItemMax = 10;
+
+    //背景透明
+    this->setAttribute(Qt::WA_TranslucentBackground);
+    //设置无边框
+    this->setWindowFlags(Qt::FramelessWindowHint);
+
+    //注册热键必须在setAttribute(Qt::WA_TranslucentBackground)之后, 否则会导致透明色变成黑色
     m_HotKeyShow = GlobalAddAtom(TEXT("showSyaringan")) - 0xC00;	//获得唯一ID()
     if (!RegisterHotKey((HWND)this->winId(), m_HotKeyShow, MOD_CONTROL, 0))
     {
         QMessageBox message(QMessageBox::Information, "警告", "注册全局热键失败");
         message.exec();
     }
-    connect(ui->lineEditInput, SIGNAL(textChanged(QString)), this, SLOT(textChangedSlot(QString)));
-    m_showItemMax = 10;
-    //设置无边框
-    this->setWindowFlags(Qt::FramelessWindowHint);
-    //背景透明
-    this->setAttribute(Qt::WA_TranslucentBackground);
 }
 
 SyaringanWidget::~SyaringanWidget()
@@ -76,14 +80,13 @@ bool SyaringanWidget::nativeEvent(const QByteArray &eventType, void *message, lo
                     }
                     else
                     {
-                        //qDebug() << "show...";
-                        //this->setWindowFlags(Qt::WindowStaysOnBottomHint);
                         //设置无边框
-                        this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnBottomHint);
-                        //背景透明
-                        this->setAttribute(Qt::WA_TranslucentBackground);
+                        this->setWindowFlags(Qt::FramelessWindowHint);
+                        //设置窗口置顶
+                        ::SetWindowPos(HWND(this->winId()), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+                        ::SetWindowPos(HWND(this->winId()), HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
                         this->show();
-
+                        this->activateWindow();
                     }
                     m_lastTime = QTime();    //让时间初始化, 使下一次Ctrl的点击不会认为双击
                 }
